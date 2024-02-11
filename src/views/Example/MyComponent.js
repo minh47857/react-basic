1b0143eb38afa17639ad05642f31861aa913449e
Nguyen Minh <minh47857@gmail.com>
2024-02-12 00:42:24 +0700

    feat::component

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>My name is Minh</div>
+        )
+    }
+}
+
+export default MyComponent;
