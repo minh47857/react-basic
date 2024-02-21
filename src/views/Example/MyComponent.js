70c03db64a2d104ae8a77235315b1cef86e3f395
Nguyen Minh <minh47857@gmail.com>
2024-02-21 16:05:49 +0700

    feat: state

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+    state = {
+        name: "Nguyen Minh",
+        university: "HUST"
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    
     render() {
         return (
-            <div>My name is Minh</div>
+            <>
+                <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)}/>
+                <div>My name is {this.state.name}  <p>I'm styding at {this.state.university}</p></div>
+            </>
         )
     }
 }
