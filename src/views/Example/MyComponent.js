6a1336f69d1b1a1cc5dd6c5dd856a9d7fc4d0b13
Nguyen Minh <minh47857@gmail.com>
2024-02-22 10:19:36 +0700

    feat: form

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,21 +2,32 @@ import React from 'react';
 
 class MyComponent extends React.Component {
     state = {
-        name: "Nguyen Minh",
-        university: "HUST"
+        firstName: '',
+        lastName: '',
     }
 
-    handleOnChangeName = (event) => {
+    handleOnChangeFirstName = (event) => {
         this.setState({
-            name: event.target.value
+            firstName: event.target.value
         })
     }
-    
+
+    handleOnChangeLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
     render() {
         return (
             <>
-                <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)}/>
-                <div>My name is {this.state.name}  <p>I'm styding at {this.state.university}</p></div>
+                <form>
+                    <label htmlFor="fname">First name:</label><br />
+                    <input type="text" value= {this.state.firstName} onChange={event => this.handleOnChangeFirstName(event)}/><br />
+                    <label htmlFor="lname">Last name:</label><br />
+                    <input type="text"  value= {this.state.lastName} onChange={event => this.handleOnChangeLastName(event)} /><br /><br />
+                    <input type="submit" value="Submit" />
+                </form>
             </>
         )
     }
